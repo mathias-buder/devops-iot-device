757c35de38ed496d988e99ef8813183cb33d1bb6
Mathias Buder <mathias@sewela.com>
2020-06-15 08:44:05 +0200

    [DD] Added I2C address details

--- 010_Main_Application/DD/Config/dd_pga-302_cfg.h
+++ 010_Main_Application/DD/Config/dd_pga-302_cfg.h
@@ -21,7 +21,26 @@
 /*************************************************************/
 /*      INCLUDES                                             */
 /*************************************************************/
+#include "../Core/dd_types.h"
 
+
+/*************************************************************/
+/*      GLOBAL DEFINES                                       */
+/*************************************************************/
+/* Log message TAG string */
+#define DD_PGA_302_LOG_MSG_TAG                    "DD_PGA_302"
+
+/* I2C configuration
+ * The I2C digital interface (DI) is organized in different memory
+ * pages which requires dedicated address offsets to access the
+ * a certain memory page */
+
+#define DD_PGA_302_I2C_ADDR_TEST_REG              0x40     /* PGA-302 I2C slave address: test registers */
+#define DD_PGA_302_I2C_ADDR_DI_OFFSET_2           0x42     /* PGA-302 I2C slave address: test control and status register, offset: 0x02 */
+#define DD_PGA_302_I2C_ADDR_DI_OFFSET_7           0x47     /* PGA-302 I2C slave address: test control and status register, offset: 0x07 */
+#define DD_PGA_302_I2C_ADDR_DI_OFFSET_EEPROM      0x45     /* PGA-302 I2C address offset: EEPROM cach + cells */
+
+// #define DD_PGA_302_DEVICE_ID                      0x00 /* Unique device id */
 
 /*************************************************************/
 /*      GLOBAL VARIABLES                                     */
