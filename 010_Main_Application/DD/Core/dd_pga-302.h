f724fd2d1f51d7e7174565aa2ccdd4bceedebf0a
Mathias Buder <mathias@sewela.com>
2020-08-01 00:14:25 +0200

    [DD] Added initial PGA302 register defines

--- 010_Main_Application/DD/Core/dd_pga-302.h
+++ 010_Main_Application/DD/Core/dd_pga-302.h
@@ -28,7 +28,32 @@
 
 
 /*************************************************************/
+/*      GLOBAL DEFINES                                       */
+/*************************************************************/
+
+#define DD_PGA302_P_GAIN_SELECT                 0x47
+#define DD_PGA302_T_GAIN_SELECT                 0x48
+#define DD_PGA302_TEMP_CTRL                     0x4C
+#define DD_PGA302_OFFSET_CANCE_L                0x4E
+#define DD_PGA302_PADC_DATA1                    0x20
+#define DD_PGA302_PADC_DATA2                    0x21
+#define DD_PGA302_TADC_DATA1                    0x24
+#define DD_PGA302_TADC_DATA2                    0x25
+#define DD_PGA302_DAC_REG0_1                    0x30
+#define DD_PGA302_DAC_REG0_2                    0x31
+#define DD_PGA302_OP_STAGE_CTR_L                0x3B
+#define DD_PGA302_PSMON1                        0x58
+#define DD_PGA302_AFEDIAG                       0x5A
+#define DD_PGA302_MICRO_IF_CTL                  0x0C
+
+
+/*************************************************************/
 /*      GLOBAL VARIABLES                                     */
+/*************************************************************/
+
+
+/*************************************************************/
+/*      PROTOTYPES                                           */
 /*************************************************************/
 BOOLEAN dd_pga_302_init( void );
 void    dd_pga_302_main( void );
