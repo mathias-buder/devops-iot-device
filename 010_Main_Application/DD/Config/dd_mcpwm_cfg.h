022845fd3d9a76c6f5f950374a6302ff153a0281
Mathias Buder <mathias.buder@zf.com>
2020-09-12 20:09:03 +0200

    [DD] Initial MCPWM file setup

---
+++ 010_Main_Application/DD/Config/dd_mcpwm_cfg.h
@@ -0,0 +1,40 @@
+/*********************************************************************
+
+        SEWELA owns the copyright in this document and associated
+        documents and all rights are reserved. These documents must
+        not be used for any purpose other than that for which they
+        are supplied and must not be copied in whole or in part or
+        disclosed to others without prior written consent of SEWELA.
+        Any copy of this drawing or document made by any method
+        must also include a copy of this legend.
+
+        @file dd_mcpwm_cfg.c
+        @details Some detailed description
+
+        (c) SEWELA 2020
+
+*********************************************************************/
+
+#ifndef DD_MCPWM_CFG_H
+#define DD_MCPWM_CFG_H
+
+/*************************************************************/
+/*      INCLUDES                                             */
+/*************************************************************/
+//#include "driver/adc.h"
+//#include "driver/gpio.h"
+
+#include "../../types.h"
+
+/*************************************************************/
+/*      GLOBAL DEFINES                                       */
+/*************************************************************/
+/* Log message TAG string */
+#define DD_MCPWM_LOG_MSG_TAG              "DD_MCPWM"
+
+/*************************************************************/
+/*      GLOBAL VARIABLES                                     */
+/*************************************************************/
+
+
+#endif /* DD_MCPWM_CFG_H */
---
+++ 010_Main_Application/DD/Core/dd_mcpwm.c
@@ -0,0 +1,56 @@
+/*********************************************************************
+
+        SEWELA owns the copyright in this document and associated
+        documents and all rights are reserved. These documents must
+        not be used for any purpose other than that for which they
+        are supplied and must not be copied in whole or in part or
+        disclosed to others without prior written consent of SEWELA.
+        Any copy of this drawing or document made by any method
+        must also include a copy of this legend.
+
+        @file dd_mcpwm.c
+        @details Some detailed description
+
+        (c) SEWELA 2020
+
+*********************************************************************/
+
+/*********************************************************************/
+/*      INCLUDES                                                     */
+/*********************************************************************/
+#include <stdio.h>
+#include <string.h>
+
+#include "esp_log.h"
+
+#include "dd_mcpwm.h"
+#include "dd_database.h"
+
+#include "../../types.h"
+
+/*********************************************************************/
+/*      GLOBAL VARIABLES                                             */
+/*********************************************************************/
+
+
+/*********************************************************************/
+/*      PRIVATE FUNCTION DECLARATIONS                                */
+/*********************************************************************/
+
+/*********************************************************************/
+/*   FUNCTION DEFINITIONS                                            */
+/*********************************************************************/
+
+BOOLEAN dd_mcpwm_init( void )
+{
+
+
+    return TRUE;
+}
+
+void dd_mcpwm_main( void )
+{
+
+}
+
+
