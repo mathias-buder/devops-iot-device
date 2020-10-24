fbadc12c32c11eafda8cc0494a62d004dcf06c9e
Mathias Buder <mathias@sewela.com>
2020-10-25 01:02:15 +0200

    [SIM] Fixed wrong integer-type mapping within function SetSignal()

--- 020_Test/010_Gogeta/scripts/sbf2blf_converter/CanMessage.py
+++ 020_Test/010_Gogeta/scripts/sbf2blf_converter/CanMessage.py
@@ -10,7 +10,7 @@ class CanMessage:
     MaxLengthInBit = 64  # Maximum size of the message in bit
 
     # Constructor
-    def __init__(self, CanId, name):
+    def __init__(self, name, CanId):
         self.CanId = CanId
         self.name = name
         self.CurrentLengthInBit = 0            # Current size of the message in bit
@@ -53,21 +53,10 @@ class CanMessage:
         # Search for corresponding signal
         for signal in self.signals:
             if signal['name'] == name:
-
                 # Convert signal according to scaling factor and offset
                 RawVal = (1.0 / signal['ScaleFactor']) * (value - signal['offset'])
-
                 # Cast raw value to corresponding integer
                 if(True == signal['signed']):
-                    if(signal['LengthInBit'] <= 8):
-                        RawVal = np.uint8(RawVal)
-                    elif (signal['LengthInBit'] <= 16):
-                        RawVal = np.uint16(RawVal)
-                    elif (signal['LengthInBit'] <= 32):
-                        RawVal = np.uint32(RawVal)
-                    else:
-                        RawVal = np.uint64(RawVal)
-                else:
                     if(signal['LengthInBit'] <= 8):
                         RawVal = np.int8(RawVal)
                     elif (signal['LengthInBit'] <= 16):
@@ -76,14 +65,24 @@ class CanMessage:
                         RawVal = np.int32(RawVal)
                     else:
                         RawVal = np.int64(RawVal)
+                else:
+                    if(signal['LengthInBit'] <= 8):
+                        RawVal = np.uint8(RawVal)
+                    elif (signal['LengthInBit'] <= 16):
+                        RawVal = np.uint16(RawVal)
+                    elif (signal['LengthInBit'] <= 32):
+                        RawVal = np.uint32(RawVal)
+                    else:
+                        RawVal = np.uint64(RawVal)
 
                 # Cast raw value to corresponding bit-length
                 signal['RawValueCasted'] = signal['BitMask'] & RawVal
 
                 # Build / Rebuild 64 bit message data frame
                 self.__UpdateDataFrame()
+                return True
 
-                break
+        print('{}: Signal {} not found!' .format(self.name, name))
 
 
     def GetId(self):
@@ -112,13 +111,13 @@ class CanMessage:
 
     def GetDataFrame(self, PrintFrame=False):
         if(True == PrintFrame):
-            print('Data frame: {}'.format(np.binary_repr( self.CanDataFrame_u64, width=self.MaxLengthInBit)))
+            print('{0:>30}: {1:>64}'.format('Data frame', np.binary_repr( self.CanDataFrame_u64, width=self.MaxLengthInBit)))
         return self.CanDataFrame_u64
 
 
 
-DD_INA_219_Data_A = CanMessage(30, 'DD_INA_219_Data_A')
-DD_INA_219_Data_B = CanMessage(31, 'DD_INA_219_Data_B')
+DD_INA_219_Data_A = CanMessage('DD_INA_219_Data_A', 30)
+DD_INA_219_Data_B = CanMessage('DD_INA_219_Data_B', 31)
 
 DD_INA_219_Data_A.AddSignal('DD_INA_219_bus_voltage_raw',     False, 1, 0.0, 16, 0)
 DD_INA_219_Data_A.AddSignal('DD_INA_219_shunt_voltage_raw',   False, 1, 0.0, 16, 16)
@@ -127,19 +126,27 @@ DD_INA_219_Data_A.AddSignal('DD_INA_219_power_raw',           False, 1, 0.0, 16,
 DD_INA_219_Data_A.PrintSignals()
 DD_INA_219_Data_A.PrintSignalLayout()
 
-DD_INA_219_Data_B.AddSignal('DD_INA_219_current',             False, 1,   0.0, 7, 0)
-DD_INA_219_Data_B.AddSignal('DD_INA_219_bus_voltage',         False, 0.1, 0.0, 8, 8)
-DD_INA_219_Data_B.AddSignal('DD_INA_219_shunt_voltage',       False, 1,   0.0, 2, 16)
-DD_INA_219_Data_B.AddSignal('DD_INA_219_bus_voltage_range',   False, 1,   0.0, 1, 24)
-DD_INA_219_Data_B.AddSignal('DD_INA_219_shunt_voltage_range', False, 0.1, 0.0, 9, 32)
-DD_INA_219_Data_B.AddSignal('DD_INA_219_power',               False, 1,   0.0, 10, 48)
+DD_INA_219_Data_A.SetSignal('DD_INA_219_bus_voltage_raw', 10)
+DD_INA_219_Data_A.SetSignal('DD_INA_219_current_raw', 16)
+
+DD_INA_219_Data_A.GetDataFrame(True)
+
+
+
+DD_INA_219_Data_B.AddSignal('DD_INA_219_current',             False, 0.1, 0.0, 14, 0)
+DD_INA_219_Data_B.AddSignal('DD_INA_219_shunt_voltage_range', False, 1,   0.0, 2,  14)
+DD_INA_219_Data_B.AddSignal('DD_INA_219_power',               False, 0.1, 0.0, 14, 16)
+DD_INA_219_Data_B.AddSignal('DD_INA_219_bus_voltage_range',   False, 1,   0.0, 1,  30)
+DD_INA_219_Data_B.AddSignal('DD_INA_219_shunt_voltage',       False, 0.1, 0.0, 9,  32)
+DD_INA_219_Data_B.AddSignal('DD_INA_219_bus_voltage',         False, 0.1, 0.0, 8,  48)
+
 DD_INA_219_Data_B.PrintSignals()
 DD_INA_219_Data_B.PrintSignalLayout()
 
-DD_INA_219_Data_B.SetSignal('DD_INA_219_current', 8)
+DD_INA_219_Data_B.SetSignal('DD_INA_219_current', 234.5)
 DD_INA_219_Data_B.SetSignal('DD_INA_219_bus_voltage', 10)
 
-DataFrame = DD_INA_219_Data_B.GetDataFrame(True)
+DD_INA_219_Data_B.GetDataFrame(True)
 
 
 
