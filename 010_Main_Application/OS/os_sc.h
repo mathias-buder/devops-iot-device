e3ad9c29db025722d4bbc5a1e9fde7ea10ae6728
Mathias Buder <mathias.buder@zf.com>
2020-11-02 13:43:54 +0100

    [OS] Initial file setup

---
+++ 010_Main_Application/OS/os_sc.h
@@ -0,0 +1,30 @@
+/*********************************************************************
+
+        SEWELA owns the copyright in this document and associated
+        documents and all rights are reserved. These documents must
+        not be used for any purpose other than that for which they
+        are supplied and must not be copied in whole or in part or
+        disclosed to others without prior written consent of SEWELA.
+        Any copy of this drawing or document made by any method
+        must also include a copy of this legend.
+
+        (c) SEWELA 2020
+
+        @file os_sc.h
+
+*********************************************************************/
+#ifndef OS_SC_H_
+#define OS_SC_H_
+
+/*************************************************************/
+/*      INCLUDES                                             */
+/*************************************************************/
+#include "../types.h"
+
+/*************************************************************/
+/*      GLOBAL VARIABLES                                     */
+/*************************************************************/
+extern void os_sc_init( void );
+extern void os_sc_main( void );
+
+#endif /* OS_SC_H_ */
