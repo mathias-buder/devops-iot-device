7e190060ffa76f8d0c5a8090fdd488fff7fce70c
Mathias Buder <mathias@sewela.com>
2020-12-31 11:03:56 +0100

    [SIM] Added option to enable/disable the generation of data structure _wrapper_s (now default OFF)

--- 020_Test/SeWelaSim/sewela_sim.cpp
+++ 020_Test/SeWelaSim/sewela_sim.cpp
@@ -1,32 +1,91 @@
-#include "sewela_sim.h"
+/*********************************************************************
+    MODULE SPECIFICATION:
 
-#include "../../010_Construction/MainApplication/SENSE/SENSE.h"
-#include "../../010_Construction/MainApplication/VE/VE.h"
-#include "../../010_Construction/MainApplication/UTIL/UTIL.h"
+        SEWELA owns the copyright in this document and associated
+        documents and all rights are reserved. These documents must
+        not be used for any purpose other than that for which they
+        are supplied and must not be copied in whole or in part or
+        disclosed to others without prior written consent of SEWELA.
+        Any copy of this drawing or document made by any method
+        must also include a copy of this legend.
 
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
+        (c) SEWELA 2020
 
+        @file sewela_sim.cpp
+        @details Contains all heard-rate related functions
+
+*********************************************************************/
+
+/*********************************************************************/
+/*      INCLUDES                                                     */
+/*********************************************************************/
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-#include "generated_src/wrapper.h"
+/* #include "generated_src/wrapper.h" */
 
 #ifdef __cplusplus
 }
 #endif
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "sewela_sim.h"
+
+
+/*************************************************************/
+/*      GLOBAL VARIABLES                                     */
+/*************************************************************/
+LogMsgPtr               p_gogeta_logger;
 
 SENSE_DATA_IN_TYPE_TAG  _read_sense_data_in_s;
 SENSE_DATA_OUT_TYPE_TAG _sense_data_out_s;
 VE_DATA_IN_TYPE_TAG     _ve_data_in_s;
 VE_DATA_OUT_TYPE_TAG    _ve_data_out_s;
 
+/*************************************************************/
+/*      FUNCTION DEFINITION                                  */
+/*************************************************************/
+void registerLogMsg( LogMsgPtr logger )
+{
+    p_gogeta_logger = logger;
+}
+
+
+extern "C" void message_logger( char* p_expression_u8, char* p_file_path_u8, int line_s32, int critical_level_flag_s32 )
+{
+    /* make a buffer for combining the text including expression message, file path and code line */
+    char text[MAXIMUM_FILE_LENGTH + MAXIMUM_EXPRESSION_LENGTH + 50U];
+
+    unsigned int file_name_index = strlen( p_file_path_u8 ) - MAXIMUM_FILE_LENGTH - 1U;
+
+    /* if the path is larger than maximum, then the string is cut from right hand side to keep at least the file name
+     * the expression name will be cut from left hand side in case larger than maximum*/
+    if ( MAXIMUM_FILE_LENGTH > strlen( p_file_path_u8 ) )
+    {
+        /* the message is smaller than the limit, so no modification is required*/
+        snprintf( text, sizeof( text ), "issue detected: %.*s in file %s, line %d.", (int) min( MAXIMUM_EXPRESSION_LENGTH, strlen( p_expression_u8 ) ), p_expression_u8, p_file_path_u8, line_s32 );
+    }
+    else
+    {
+        snprintf( text, sizeof( text ), "issue detected: %.*s in file ...%s, line %d.", (int) min( MAXIMUM_EXPRESSION_LENGTH, strlen( p_expression_u8 ) ), p_expression_u8, &p_file_path_u8[file_name_index], line_s32 );
+    }
+    /* make sure that the pointer is not empty */
+    if ( NULL != p_gogeta_logger )
+    {
+        /* call the function pointer */
+        p_gogeta_logger( "libSeWelaSim", text, critical_level_flag_s32 );
+    }
+}
+
+
 bool init()
 {
-    init__wrapper_s();
+    /* init__wrapper_s(); */
 
     SENSE_C::init(); /* Initialize Sensor Processing Domain ( SENSE ) */
     VE_C::init();    /* Initialize Vibration Engine Domain ( VE ) */
@@ -41,43 +100,7 @@ bool runCycle( void )
     _sense_data_out_s = SENSE_C::main( _read_sense_data_in_s );
     _ve_data_out_s    = VE_C::main( _ve_data_in_s );
 
-    copy__wrapper_s();
+    /* copy__wrapper_s(); */
 
     return true;
-}
-
-
-
-LogMsgPtr gogeta_Logger = 0;
-
-void registerLogMsg( LogMsgPtr logger )
-{
-
-    gogeta_Logger = logger;
-}
-
-extern "C" void message_logger( char* expression, char* filePath, int line, int critical_level_flag )
-{
-
-    /* make a buffer for combining the text including expression message, file path and code line */
-    char text[MAXIMUM_FILE_LENGTH + MAXIMUM_EXPRESSION_LENGTH + 50U];
-
-    unsigned int file_name_index = strlen( filePath ) - MAXIMUM_FILE_LENGTH - 1U;
-
-    /* if the path is larger than maximum, then the string is cut from right hand side to keep at least the file name
-         * the expression name will be cut from left hand side in case larger than maximum*/
-
-    if ( MAXIMUM_FILE_LENGTH > strlen( filePath ) )
-    { /* the message is smaller than the limit, so no modification is required*/
-        snprintf( text, sizeof( text ), "issue detected: %.*s in file %s, line %d.", (int) min( MAXIMUM_EXPRESSION_LENGTH, strlen( expression ) ), expression, filePath, line );
-    }
-    else
-    {
-        snprintf( text, sizeof( text ), "issue detected: %.*s in file ...%s, line %d.", (int) min( MAXIMUM_EXPRESSION_LENGTH, strlen( expression ) ), expression, &filePath[file_name_index], line );
-    }
-    /* make sure that the pointer is not empty */
-    if ( NULL != gogeta_Logger )
-    {
-        gogeta_Logger( "libWeGetASim", text, critical_level_flag ); /* call the function pointer */
-    }
 }
--- 020_Test/SeWelaSim/sewela_sim.h
+++ 020_Test/SeWelaSim/sewela_sim.h
@@ -1,22 +1,61 @@
-#ifndef ECU_CONTROL_H
-#define ECU_CONTROL_H
+/*********************************************************************
+    MODULE SPECIFICATION:
+
+        SEWELA owns the copyright in this document and associated
+        documents and all rights are reserved. These documents must
+        not be used for any purpose other than that for which they
+        are supplied and must not be copied in whole or in part or
+        disclosed to others without prior written consent of SEWELA.
+        Any copy of this drawing or document made by any method
+        must also include a copy of this legend.
+
+        (c) SEWELA 2020
+
+        @file sewela_sim.h
+        @details Some detailed description
+
+*********************************************************************/
+#ifndef SEWELA_SIM_H
+#define SEWELA_SIM_H
+
+/*************************************************************/
+/*      INCLUDES                                             */
+/*************************************************************/
+#include "../../010_Construction/MainApplication/SENSE/SENSE.h"
+#include "../../010_Construction/MainApplication/UTIL/UTIL.h"
+#include "../../010_Construction/MainApplication/VE/VE.h"
 
 #include <stdbool.h>
 
-#define MAXIMUM_FILE_LENGTH         50U
-#define MAXIMUM_EXPRESSION_LENGTH   50U
+/*************************************************************
+*      DEFINES                                               *
+*************************************************************/
+#define MAXIMUM_FILE_LENGTH       50U
+#define MAXIMUM_EXPRESSION_LENGTH 50U
 
-typedef void(*LogMsgPtr)(const char* module, const char* message, int level);
+/*************************************************************/
+/*      GLOBAL VARIABLES                                     */
+/*************************************************************/
+extern SENSE_DATA_IN_TYPE_TAG  _read_sense_data_in_s;
+extern SENSE_DATA_OUT_TYPE_TAG _sense_data_out_s;
+extern VE_DATA_IN_TYPE_TAG     _ve_data_in_s;
+extern VE_DATA_OUT_TYPE_TAG    _ve_data_out_s;
+
+/*************************************************************/
+/*      FUNCTION PROTOTYPED                                  */
+/*************************************************************/
+typedef void ( *LogMsgPtr )( const char* module, const char* message, int level );
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif /* __cplusplus */
 
-__declspec(dllexport) bool init();
-__declspec(dllexport) bool runCycle();
-__declspec(dllexport) void registerLogMsg(LogMsgPtr);
+    __declspec( dllexport ) bool init();
+    __declspec( dllexport ) bool runCycle();
+    __declspec( dllexport ) void registerLogMsg( LogMsgPtr );
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
-#endif /* ECU_CONTROL_H */
+#endif /* SEWELA_SIM_H */
