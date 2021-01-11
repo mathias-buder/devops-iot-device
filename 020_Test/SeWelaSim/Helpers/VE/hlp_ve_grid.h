c85a40c4aee2264145a0ba5f6809a8a581a347e3
Mathias Buder <mathias.buder@zf.com>
2021-01-11 23:10:35 +0100

    [SIM] Added Helper directory and added it CMakeList.txt

---
+++ 020_Test/SeWelaSim/Helpers/VE/hlp_ve_grid.h
@@ -0,0 +1,31 @@
+/*********************************************************************
+
+        SEWELA owns the copyright in this document and associated
+        documents and all rights are reserved. These documents must
+        not be used for any purpose other than that for which they
+        are supplied and must not be copied in whole or in part or
+        disclosed to others without prior written consent of SEWELA.
+        Any copy of this drawing or document made by any method
+        must also include a copy of this legend.
+
+        @file hlp_ve_grid.h
+        @details Vibration grid module helper functions implementation
+
+        (c) SEWELA 2020
+
+*********************************************************************/
+#ifndef HLP_VE_GRID_H_
+#define HLP_VE_GRID_H_
+
+/*************************************************************/
+/*      INCLUDES                                             */
+/*************************************************************/
+#include "../types.h"
+
+/*************************************************************/
+/*      COMPILE TIME CONFIGURATION                           */
+/*************************************************************/
+
+
+
+#endif /* HLP_VE_GRID_H_ */
